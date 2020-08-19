c661dde956f9bd216a85376d0e089b868a1ad5d2
Alex Kosh <sasha23969@gmail.com>
2020-08-19 23:10:55 +0300

    test: Test that API secrets exist

---
+++ test/test_env.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import unittest
+import os
+
+
+class TestEnvVariables(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        from dotenv import load_dotenv, find_dotenv
+        load_dotenv(find_dotenv())
+
+    def test_telethon_api_keys(self):
+        TELETHON_API_ID = os.environ.get("TELETHON_API_ID")
+        TELETHON_API_HASH = os.environ.get("TELETHON_API_HASH")
+
+        self.assertIsNotNone(TELETHON_API_ID)
+        self.assertIsNotNone(TELETHON_API_HASH)
+
+    def test_telegram_bot_token(self):
+        TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+
+        self.assertIsNotNone(TELEGRAM_BOT_TOKEN)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
