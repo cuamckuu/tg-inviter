312390a2f7f330be9b126dd0c867dc0c130efe37
Alex Kosh <sasha23969@gmail.com>
2020-08-20 03:30:34 +0300

    test: Test for MemoryStorage class

---
+++ test/test_storage.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import unittest
+import uuid
+
+if __name__ == '__main__':
+    __import__("sys").path.append("./../")
+
+from tg_inviter.storage import BaseStorage, MemoryStorage
+
+
+class TestMemoryStorage(unittest.TestCase):
+
+    def setUp(self):
+        self.storage = MemoryStorage()
+
+    def test_storage(self):
+        token = self.storage.generate_token()
+
+        self.assertEqual(self.storage.uses_left(token), 0)
+
+        self.storage.insert(token, 5)
+
+        self.assertEqual(self.storage.uses_left(token), 5)
+
+        for _ in range(5):
+            self.storage.count_new_use(token)
+
+        self.assertEqual(self.storage.uses_left(token), 0)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
