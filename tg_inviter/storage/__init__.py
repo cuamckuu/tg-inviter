6a8049249d52ef9aaa5da8fb630401bdc76bb3af
Alex Kosh <sasha23969@gmail.com>
2020-08-20 03:42:48 +0300

    feat: Added MemoryStorage class

---
+++ tg_inviter/storage/__init__.py
@@ -0,0 +1,4 @@
+from tg_inviter.storage.base_storage import BaseStorage
+from tg_inviter.storage.memory_storage import MemoryStorage
+
+__all__ = ["BaseStorage", "MemoryStorage"]
---
+++ tg_inviter/storage/memory_storage.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+from tg_inviter.storage.base_storage import BaseStorage
+
+
+class MemoryStorage(BaseStorage):
+    """Store invite tokens inside in-memory dictionary"""
+
+    def __init__(self):
+        """Create in-memory dict to store invite tokens"""
+
+        self.storage = defaultdict(lambda: [0, 0])
+
+    def insert(self, token, max_uses):
+        self.storage[token][1] = max_uses
+
+    def uses_left(self, token):
+        return max(0, self.storage[token][1] - self.storage[token][0])
+
+    def count_new_use(self, token):
+        self.storage[token][0] += 1
