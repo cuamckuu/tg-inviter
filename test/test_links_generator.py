caf1f053810301523f9a169d21206f96ae44dea5
Alex Kosh <sasha23969@gmail.com>
2020-08-23 23:33:03 +0300

    test: Require token as kwarg for generate_invite_link

--- test/test_links_generator.py
+++ test/test_links_generator.py
@@ -20,47 +20,47 @@ class TestLinksGeneration(unittest.TestCase):
         with self.assertRaises(ValueError):
             generate_invite_link(self.bot_name, proto="qwe")
 
-        generate_invite_link(self.bot_name, self.token, proto="tg")
-        generate_invite_link(self.bot_name, self.token, proto="http")
-        generate_invite_link(self.bot_name, self.token, proto="https")
+        generate_invite_link(self.bot_name, token=self.token, proto="tg")
+        generate_invite_link(self.bot_name, token=self.token, proto="http")
+        generate_invite_link(self.bot_name, token=self.token, proto="https")
 
     def test_return_token(self):
         link, token = generate_invite_link(self.bot_name)
 
         self.assertTrue(link.endswith(token))
 
-    def test_generate_full_link(self):
+    def test_generate_full_deeplink(self):
         http = f"http://telegram.me/{self.bot_name}?start={self.token}"
         https = f"https://telegram.me/{self.bot_name}?start={self.token}"
 
-        link, _ = generate_invite_link(self.bot_name, self.token, short=False)
+        link, _ = generate_invite_link(self.bot_name, token=self.token, short=False)
         self.assertEqual(link, https)
 
         link, _ = generate_invite_link(
-            self.bot_name, self.token, short=False, proto="https"
+            self.bot_name, token=self.token, short=False, proto="https"
         )
         self.assertEqual(link, https)
 
         link, _ = generate_invite_link(
-            self.bot_name, self.token, short=False, proto="http"
+            self.bot_name, token=self.token, short=False, proto="http"
         )
         self.assertEqual(link, http)
 
-    def test_generate_short_link(self):
+    def test_generate_short_deeplink(self):
         http = f"http://t.me/{self.bot_name}?start={self.token}"
         https = f"https://t.me/{self.bot_name}?start={self.token}"
 
-        link, _ = generate_invite_link(self.bot_name, self.token)
+        link, _ = generate_invite_link(self.bot_name, token=self.token)
         self.assertEqual(link, https)
 
-        link, _ = generate_invite_link(self.bot_name, self.token, short=True)
+        link, _ = generate_invite_link(self.bot_name, token=self.token, short=True)
         self.assertEqual(link, https)
 
-        link, _ = generate_invite_link(self.bot_name, self.token, proto="http")
+        link, _ = generate_invite_link(self.bot_name, token=self.token, proto="http")
         self.assertEqual(link, http)
 
         link, _ = generate_invite_link(
-            self.bot_name, self.token, proto="https"
+            self.bot_name, token=self.token, proto="https"
         )
         self.assertEqual(link, https)
 
@@ -70,7 +70,7 @@ class TestLinksGeneration(unittest.TestCase):
 
         self.assertNotEqual(token1, token2)
 
-    def test_random_links(self):
+    def test_random_deeplinks(self):
         https = f"https://t.me/{self.bot_name}?start="
 
         link1, token1 = generate_invite_link(self.bot_name)
@@ -82,18 +82,18 @@ class TestLinksGeneration(unittest.TestCase):
         self.assertNotEqual(token1, token2)
         self.assertNotEqual(link1, link2)
 
-    def test_generate_tg_proto_link(self):
+    def test_generate_tg_proto_deeplink(self):
         tg = f"tg://resolve?domain={self.bot_name}&start={self.token}"
 
-        link, _ = generate_invite_link(self.bot_name, self.token, proto="tg")
+        link, _ = generate_invite_link(self.bot_name, token=self.token, proto="tg")
         self.assertEqual(link, tg)
 
         link, _ = generate_invite_link(
-            self.bot_name, self.token, proto="tg", short=True
+            self.bot_name, token=self.token, proto="tg", short=True
         )
         self.assertEqual(link, tg)
 
         link, _ = generate_invite_link(
-            self.bot_name, self.token, proto="tg", short=False
+            self.bot_name, token=self.token, proto="tg", short=False
         )
         self.assertEqual(link, tg)
