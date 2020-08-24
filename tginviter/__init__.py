696c7237b228a431bd1d27ebbe3d04a109f62f2f
Alex Kosh <sasha23969@gmail.com>
2020-08-24 23:19:51 +0300

    fix: Better imports

--- tginviter/__init__.py
+++ tginviter/__init__.py
@@ -1,4 +1,8 @@
 from tginviter.links_generator import generate_invite_link, \
         get_random_token, generate_joinchat_link
 
+import tginviter.bot as bot
+import tginviter.storage as storage
+import tginviter.client as client
+
 __all__ = ["generate_invite_link", "get_random_token", "generate_joinchat_link"]
